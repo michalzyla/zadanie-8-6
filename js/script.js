9642e5cfc5b72e6c1e04efe4b1173f35137c9d01
Michał Żyła <michalzyla7@gmail.com>
2018-01-05 17:09:49 +0100

    Init project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 120;
+var value = (a * a) - (2 * a * b) + (b * b);
+if(value < 0) {
+	result = "wynik ujemny";
+} else if(value = 0) {
+	result = "wynik wynosi 0";
+} else {
+	result = "wynik dodatni";
+}
+console.log(result);
